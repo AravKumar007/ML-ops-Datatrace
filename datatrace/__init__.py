9603db3f968a07f1fc925124a798f991495c3caf
Arav <aravkumar797@gmail.com>
2026-01-14 16:10:41 +0530

    updated __init__.py

--- datatrace/__init__.py
+++ datatrace/__init__.py
@@ -1,29 +1,41 @@
 # datatrace/__init__.py
-# Expose key functions at package level for easy import: from datatrace import add_dataset, etc.
+"""
+Make the most important functions available directly:
+from datatrace import add_dataset, list_datasets, log_experiment, ...
+"""
 
-# Hashing & core utilities
+# Hashing & versioning
 from .core import file_hash, dataset_hash, version_id
 
-# Dataset management
-from .versioning import add_dataset, list_datasets   # ← Correct location!
+# Dataset operations (most important ones are actually here)
+from .versioning import add_dataset, list_datasets
 
 # Experiments
 from .experiments import init_experiments_table, log_experiment, get_experiments
 
-# Tracking / auditing
+# Usage tracking / auditing
 from .tracking import track_usage
 
 # Visualization
 from .visualize import visualize_metric, plot_experiments
 
-# Utils (optional exposure)
+# Optional useful utils
 from .utils import ensure_storage, now, save_json, BASE_DIR, META_DB
 
-# What gets imported with "from datatrace import *"
+# Controls what you get with "from datatrace import *"
 __all__ = [
-    'add_dataset', 'list_datasets',
-    'log_experiment', 'get_experiments',
-    'track_usage', 'visualize_metric',
-    'plot_experiments', 'file_hash', 'dataset_hash', 'version_id',
-    # Add more if you use them often
+    'add_dataset',
+    'list_datasets',
+    'log_experiment',
+    'get_experiments',
+    'track_usage',
+    'visualize_metric',
+    'plot_experiments',
+    'file_hash',
+    'dataset_hash',
+    'version_id',
+    'ensure_storage',
+    'now',
+    'save_json',
 ]
+
