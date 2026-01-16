ea697c3427226ebbf285637ac30c8e81752b68ec
Arav <aravkumar797@gmail.com>
2026-01-16 17:56:25 +0530

    Update __init__.py

--- datatrace/__init__.py
+++ datatrace/__init__.py
@@ -1,21 +1,37 @@
 # datatrace/__init__.py
+"""
+Package initializer - exposes main functions at the top level
+"""
+
+# Core utilities
 from .core import file_hash, dataset_hash, version_id
 
-# Dataset operations (correct locations)
-from .versioning import add_dataset
-from .datasets import list_datasets
+# Dataset related - pick correct location for each function
+from .versioning import add_dataset                  # assuming this is where add_dataset lives
 
+# Experiments
 from .experiments import init_experiments_table, log_experiment, get_experiments
+
+# Tracking
 from .tracking import track_usage
+
+# Visualization
 from .visualize import visualize_metric, plot_experiments
 
+# Utils
 from .utils import ensure_storage, now, save_json, BASE_DIR, META_DB
 
+
 __all__ = [
-    'add_dataset', 'list_datasets',
-    'log_experiment', 'get_experiments',
-    'track_usage', 'visualize_metric',
+    'add_dataset',
+    'list_datasets',          
+    'log_experiment',
+    'get_experiments',
+    'track_usage',
+    'visualize_metric',
     'plot_experiments',
-    'file_hash', 'dataset_hash', 'version_id',
+    'file_hash',
+    'dataset_hash',
+    'version_id',
 ]
 
