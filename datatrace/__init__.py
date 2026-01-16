a95e1d99a8c54e9a4e0300cd5e7e202b23bc6e9b
Arav <aravkumar797@gmail.com>
2026-01-16 16:25:04 +0530

    Refactor imports and __all__ in __init__.py

--- datatrace/__init__.py
+++ datatrace/__init__.py
@@ -1,47 +1,21 @@
 # datatrace/__init__.py
-"""
-Make the most important functions available directly:
-from datatrace import add_dataset, list_datasets, log_experiment, ...
-"""
-
-# Hashing & versioning
 from .core import file_hash, dataset_hash, version_id
 
-# Dataset operations (most important ones are actually here)
-from .versioning import add_dataset, list_datasets
+# Dataset operations (correct locations)
+from .versioning import add_dataset
+from .datasets import list_datasets
 
-# Experiments
 from .experiments import init_experiments_table, log_experiment, get_experiments
-
-# Usage tracking / auditing
 from .tracking import track_usage
-
-# Visualization
 from .visualize import visualize_metric, plot_experiments
 
-# Optional useful utils
 from .utils import ensure_storage, now, save_json, BASE_DIR, META_DB
 
-
-
-from .versioning import add_dataset, list_datasets  # or whatever functions you have there
-from .visualize import visualize_metric, plot_experiments  
-
-# Controls what you get with "from datatrace import *"
 __all__ = [
-    'add_dataset',
-    'list_datasets',
-    'log_experiment',
-    'get_experiments',
-    'track_usage',
-    'visualize_metric',
+    'add_dataset', 'list_datasets',
+    'log_experiment', 'get_experiments',
+    'track_usage', 'visualize_metric',
     'plot_experiments',
-    'file_hash',
-    'dataset_hash',
-    'version_id',
-    'ensure_storage',
-    'now',
-    'save_json',
+    'file_hash', 'dataset_hash', 'version_id',
 ]
-
 
