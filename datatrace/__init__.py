f0cc0de0a1d5cfdb58b3e65f4597efc32ef9cd96
Arav <aravkumar797@gmail.com>
2026-01-16 18:33:24 +0530

    Reorganize imports and update __all__ exports

--- datatrace/__init__.py
+++ datatrace/__init__.py
@@ -1,27 +1,39 @@
-# datatrace/__init__.py
+# datatrace/__init__.py - Package entry point
+
 from .core import file_hash, dataset_hash, version_id
 
-# Dataset functions (now correct locations)
+# Dataset functions
+from .datasets import hash_file, log_dataset, list_datasets
+
+# Dataset addition/versioning
 from .versioning import add_dataset
-from .datasets import list_datasets, log_dataset, hash_file
 
 # Experiments
 from .experiments import init_experiments_table, log_experiment, get_experiments
 
-# Tracking
+# Usage tracking
 from .tracking import track_usage
 
 # Visualization
 from .visualize import visualize_metric, plot_experiments
 
-# Utils
+# Utilities
 from .utils import ensure_storage, now, save_json, BASE_DIR, META_DB
 
-# What you can import with from datatrace import *
 __all__ = [
-    'add_dataset', 'list_datasets', 'log_dataset', 'hash_file',
-    'log_experiment', 'get_experiments',
-    'track_usage', 'visualize_metric', 'plot_experiments',
-    'file_hash', 'dataset_hash', 'version_id',
-    'ensure_storage', 'now', 'save_json',
+    'add_dataset',
+    'list_datasets',
+    'log_dataset',
+    'hash_file',
+    'log_experiment',
+    'get_experiments',
+    'track_usage',
+    'visualize_metric',
+    'plot_experiments',
+    'file_hash',
+    'dataset_hash',
+    'version_id',
+    'ensure_storage',
+    'now',
+    'save_json',
 ]
