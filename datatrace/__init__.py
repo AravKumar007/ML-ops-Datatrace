167bc28ca3b08caed6a16ca795caa295df05a960
Arav <aravkumar797@gmail.com>
2026-01-16 18:27:21 +0530

    Refactor imports and update __all__ in __init__.py

--- datatrace/__init__.py
+++ datatrace/__init__.py
@@ -1,13 +1,9 @@
 # datatrace/__init__.py
-"""
-Package initializer - exposes main functions at the top level
-"""
-
-# Core utilities
 from .core import file_hash, dataset_hash, version_id
 
-# Dataset related - pick correct location for each function
-from .versioning import add_dataset                  # assuming this is where add_dataset lives
+# Dataset functions (now correct locations)
+from .versioning import add_dataset
+from .datasets import list_datasets, log_dataset, hash_file
 
 # Experiments
 from .experiments import init_experiments_table, log_experiment, get_experiments
@@ -21,17 +17,11 @@ from .visualize import visualize_metric, plot_experiments
 # Utils
 from .utils import ensure_storage, now, save_json, BASE_DIR, META_DB
 
-
+# What you can import with from datatrace import *
 __all__ = [
-    'add_dataset',
-    'list_datasets',          
-    'log_experiment',
-    'get_experiments',
-    'track_usage',
-    'visualize_metric',
-    'plot_experiments',
-    'file_hash',
-    'dataset_hash',
-    'version_id',
+    'add_dataset', 'list_datasets', 'log_dataset', 'hash_file',
+    'log_experiment', 'get_experiments',
+    'track_usage', 'visualize_metric', 'plot_experiments',
+    'file_hash', 'dataset_hash', 'version_id',
+    'ensure_storage', 'now', 'save_json',
 ]
-
